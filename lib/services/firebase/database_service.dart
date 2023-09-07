ebcb9193a2f6da87b240866c1e011d78f96998d2
changhoji <onkkno1@gmail.com>
2023-09-07 18:03:50 +0900

    Remove: remove unused functions

--- lib/services/firebase/database_service.dart
+++ lib/services/firebase/database_service.dart
@@ -1,20 +1,3 @@
 import 'package:firebase_database/firebase_database.dart';
 
-class DatabaseService {
-  static Future<String> linkedHandle(String uid) async {
-    final ref = FirebaseDatabase.instance.ref("users/$uid");
-    final snapshot = await ref.child("handle").get();
-
-    if (snapshot.exists) {
-      return snapshot.toString();
-    }
-    return "";
-  }
-
-  static void test(String uid, String message) async {
-    final ref = FirebaseDatabase.instance.ref("users/$uid");
-    await ref.set({
-      "handle": "changhoji",
-    });
-  }
-}
+class DatabaseService {}
